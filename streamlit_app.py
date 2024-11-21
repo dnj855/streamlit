0d957816c939111a2056652854b2454e5e3e8a07
Cedric Lang-Roth <dnj855@yahoo.com>
2024-11-21 15:12:08 +0100

    Line chart

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,30 +1,12 @@
 import numpy as np
-import altair as alt
 import pandas as pd
 import streamlit as st
 
-st.header('st.write')
+st.header('Line chart')
 
-st.write('Hello, *World!* :sunglasses:')
-
-st.write(1234)
-
-df = pd.DataFrame({
-  'première colonne' : [1, 2, 3, 4],
-  'deuxième colonne' : [10, 20, 30, 40]
-})
-
-st.write(df)
-
-st.write('Ci-dessous se trouve un DataFrame :', df)
-
-df2 = pd.DataFrame(
-  np.random.randn(200,3),
+df = pd.DataFrame(
+  np.random.randn(20,3),
   columns=['a', 'b', 'c']
 )
 
-c = alt.Chart(df2).mark_circle().encode(
-  x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c']
-)
-
-st.write('Et ci-dessous se trouve un beau graphique aléatoire :', c)
+st.line_chart(df)
